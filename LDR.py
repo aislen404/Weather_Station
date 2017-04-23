a7592d04edb0567a1e3709e4faabba9daab3dc26
aislen404 <aislen404@gmail.com>
2017-04-23 10:03:08 +0000

    Now the new LDR

--- LDR.py
+++ LDR.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 
 
-import RPi.GPIO as GPIO, time
+from gpiozero import LightSensor
 
 class LDR(object):
     def __init__(self, pin=None):
-        self.pin = pin
+        self.ldr = LightSensor(pin)
     def get_light(self):
-        self.reading = 0
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT)
-        GPIO.output(self.pin, GPIO.LOW)
-        time.sleep(0.1)
-        GPIO.setup(self.pin, GPIO.IN)
-        while (GPIO.input(self.pin) == GPIO.LOW):
-            self.reading += 1
-        return self.reading
+        return self.ldr.value
 
