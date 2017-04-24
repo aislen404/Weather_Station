cf9220ff2e9901d5e2958f3e6099645771094da3
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2017-04-24 10:14:35 +0200

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -4,17 +4,18 @@ from pymongo import MongoClient
 
 
 class MONGO(object):
-    def __init__(self,SERVER,PORT):
+    def __init__(self,SERVER,PORT,DB):
         # creating connectioons for communicating with Mongo DB
         self._server = SERVER
         self._port = PORT
         self.client = MongoClient(self._server, self._port)
+        self.db = self.client[DB]
 
     # Function to insert data into mongo db
-    def insert(self, collection):
-        db = self.client[collection]
+    def insert(self, collection,data):
+        cllctn = self.db[collection]
         try:
-            db.insert_one({})
+            cllctn.insert_one(data)
             print '\nInserted data successfully\n'
 
         except Exception, e:
